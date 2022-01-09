938766c9a31d55adc3b4622e3c1e373ac4f012c3
Utsav Malik <utsavmalik10@gmail.com>
2022-01-08 23:18:49 -0600

    Update ViewController.swift
    fixed a bug for 20 percentage tip

--- CodePathPreWork/CodePathPreWork/ViewController.swift
+++ CodePathPreWork/CodePathPreWork/ViewController.swift
@@ -23,7 +23,7 @@ class ViewController: UIViewController {
     @IBAction func calculateTip(_ sender: Any) {
         let bill =  Double(billAmountTextField.text!) ?? 0
         
-        let tipPercentages = [0.15, 0.18, 0,2]
+        let tipPercentages = [0.15, 0.18, 0.2]
         let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
         let total = bill + tip
         
